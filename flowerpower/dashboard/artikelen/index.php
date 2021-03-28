f777853f973943e19f20ae65548fc88486a487fa
Sarai Misidjan <smisidjan@roc-dev.com>
2021-03-28 01:08:36 +0100

    artikelen showen

--- flowerpower/dashboard/artikelen/index.php
+++ flowerpower/dashboard/artikelen/index.php
@@ -22,16 +22,26 @@ include "header.html";
     </tr>
     </thead>
     <tbody>
-    <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td></td>
-        <td><span style="font-size: 20px; text-align: center; margin-right: -300px;">Bewerken</span><span data-toggle="modal" data-target="#myModal" style="cursor: pointer; float: right; margin-right: 20px;"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-</svg></span></td>
-    </tr>
+        <?php
+        $host = 'localhost';
+        $user = 'root';
+        $pass = 'root';
+        $dbnaam = "flowerpower";
+
+        $dbh = mysqli_connect($host, $user, $pass, $dbnaam);
+        if (!$dbh) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "SELECT * FROM artikel";
+        $result = $dbh -> query($sql);
+        $row_count = $result->num_rows;
+        while ($row = $result -> fetch_assoc()) {
+            echo "<tr><th>" . $row["idartikel"] . "</th><td>" . $row["naam"] . "</td><td>" . $row["omschrijving"] . "</td><td>" . $row["prijs"] . "</td><td>" . $row["categorie"] . "</td></td>";
+            echo "<td><span style='font-size: 20px; text-align: center; margin-right: -300px;'>Bewerken</span><span data-toggle='modal' data-target='#myModal' style='cursor: pointer; float: right; margin-right: 20px;'><svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
+            <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/></svg></span></td></tr>";
+        }
+        ?>
     </tbody>
 </table>
 <!-- Modal -->
