e8c0db0d735c8a3c73e4b3beba7f103224c2a617
Sarai Misidjan <smisidjan@roc-dev.com>
2021-04-06 23:49:57 +0200

    active categorie card bloemen/gelegenheid pagina

--- flowerpower/bloemen/index.php
+++ flowerpower/bloemen/index.php
@@ -6,7 +6,7 @@ require "../default/dbh.php";
 <div class="row" style="margin-top: 100px; margin-bottom: 400px;">
     <div class="column">
         <form action="index.php" method="post">
-            <div class="card active1">
+            <div class="card <?php if (isset($_POST['alle'])) { echo "active1";} ?>">
                 <button style="border: none; background-color: white;" type="submit" value="alle" name="alle" id="alle">
                     <img src="images/alle-boeketten.jpg" style="height: 200px; width: 100%;">
                     <div class="centered">Alle boeketten</div>
@@ -17,7 +17,7 @@ require "../default/dbh.php";
 
     <div class="column">
         <form action="index.php" method="post">
-            <div class="card">
+            <div class="card <?php if (isset($_POST['pluk'])) { echo "active1";} ?>">
                 <button style="border: none; background-color: white;" type="submit" value="pluk" name="pluk" id="pluk">
                     <img src="images/plukboeket.jpg" style="height: 200px; width: 100%;">
                     <div class="centered">Plukboeketten</div>
@@ -28,7 +28,7 @@ require "../default/dbh.php";
 
     <div class="column">
         <form action="index.php" method="post">
-            <div class="card">
+            <div class="card <?php if (isset($_POST['voorjaars'])) { echo "active1";} ?>">
                 <button style="border: none; background-color: white;" type="submit" value="voorjaars" name="voorjaars" id="voorjaars">
                     <img src="images/voorjaars.jpg" style="height: 200px; width: 100%;">
                     <div class="centered">Voorjaars boeketten</div>
@@ -44,22 +44,24 @@ require "../default/dbh.php";
     } elseif (isset($_POST['voorjaars'])) {
         $categorie = $_POST['voorjaars'];
         $sql = "SELECT * FROM artikel where categorie = '$categorie'";
-    } else {
+    } elseif (isset($_POST['alle'])){
         $sql = "SELECT * FROM artikel";
     }
+    if (!empty($sql)) {
+        $result = $dbh -> query($sql);
 
-    $result = $dbh -> query($sql);
-
-    if ($result) {
-        while ($row = $result -> fetch_assoc()) {
-            echo "<div class='column1'>";
-            echo "<a href='bloem.php?id=" . $row["idartikel"] . "'>";
-            echo "<div class='card1'>";
-            echo "<img style='width: 200px; height: 200px; margin-left: -30px; margin-top: -20px; position: relative; float: left;' src='../default/images/" . $row["afbeelding"] . "'>";
-            echo "<h3 class='h3'>" . $row["naam"] . "</h3>";
-            echo "</div></a></div>";
+        if ($result) {
+            while ($row = $result -> fetch_assoc()) {
+                echo "<div class='column1'>";
+                echo "<a href='bloem.php?id=" . $row["idartikel"] . "'>";
+                echo "<div class='card1'>";
+                echo "<img style='width: 200px; height: 200px; margin-left: -30px; margin-top: -20px; position: relative; float: left;' src='../default/images/" . $row["afbeelding"] . "'>";
+                echo "<h3 class='h3'>" . $row["naam"] . "</h3>";
+                echo "</div></a></div>";
+            }
         }
     }
+
     ?>
 </div>
 </body>
--- flowerpower/gelegenheid/index.php
+++ flowerpower/gelegenheid/index.php
@@ -7,7 +7,7 @@ require "../default/dbh.php";
 <div class="row">
     <div class="column">
         <form action="index.php" method="post">
-            <div class="card active1">
+            <div class="card <?php if (isset($_POST['bedankt'])) { echo "active1";} ?>">
                 <button style="border: none; background-color: white;" type="submit" value="bedankt" name="bedankt"
                         id="bedankt">
                     <img src="images/bedankt.jpg" style="height: 200px; width: 100%;">
@@ -18,7 +18,7 @@ require "../default/dbh.php";
     </div>
     <div class="column">
         <form action="index.php" method="post">
-            <div class="card">
+            <div class="card <?php if (isset($_POST['beterschap'])) { echo "active1";} ?>">
                 <button style="border: none; background-color: white;" type="submit" value="beterschap"
                         name="beterschap" id="beterschap">
                     <img src="images/beterschap.jpg" style="height: 200px; width: 100%;">
@@ -29,7 +29,7 @@ require "../default/dbh.php";
     </div>
     <div class="column">
         <form action="index.php" method="post">
-            <div class="card">
+            <div class="card <?php if (isset($_POST['geboorte'])) { echo "active1";} ?>">
                 <button style="border: none; background-color: white;" type="submit" value="geboorte" name="geboorte"
                         id="geboorte">
                     <img src="images/geboorte.jpg" style="height: 200px; width: 100%;">
@@ -43,7 +43,7 @@ require "../default/dbh.php";
     <div class="collapse" id="bekijk" style="width: 100%;">
         <div class="column">
             <form action="index.php" method="post">
-                <div class="card">
+                <div class="card <?php if (isset($_POST['gefeliciteerd'])) { echo "active1";} ?>">
                     <button style="border: none; background-color: white;" type="submit" value="gefeliciteerd"
                             name="gefeliciteerd" id="gefeliciteerd">
                         <img src="images/gefeliciteerd.jpg" style="height: 200px; width: 100%;">
@@ -54,7 +54,7 @@ require "../default/dbh.php";
         </div>
         <div class="column">
             <form action="index.php" method="post">
-                <div class="card">
+                <div class="card <?php if (isset($_POST['liefde'])) { echo "active1";} ?>">
                     <button style="border: none; background-color: white;" type="submit" value="liefde" name="liefde"
                             id="liefde">
                         <img src="images/liefde.jpg" style="height: 200px; width: 100%;">
@@ -65,7 +65,7 @@ require "../default/dbh.php";
         </div>
         <div class="column">
             <form action="index.php" method="post">
-                <div class="card">
+                <div class="card <?php if (isset($_POST['uitvaart'])) { echo "active1";} ?>">
                     <button style="border: none; background-color: white;" type="submit" value="uitvaart"
                             name="uitvaart" id="uitvaart">
                         <img src="images/uitvaart.jpg" style="height: 200px; width: 100%;">
@@ -76,8 +76,6 @@ require "../default/dbh.php";
         </div>
     </div>
     <?php
-    $sql = "SELECT * FROM artikel where categorie = 'bedankt'";
-
     if (isset($_POST['beterschap'])) {
         $categorie = $_POST['beterschap'];
         $sql = "SELECT * FROM artikel where categorie = '$categorie'";
@@ -93,22 +91,24 @@ require "../default/dbh.php";
     } elseif (isset($_POST['uitvaart'])) {
         $categorie = $_POST['uitvaart'];
         $sql = "SELECT * FROM artikel where categorie = '$categorie'";
+    } elseif (isset($_POST['bedankt'])) {
+        $sql = "SELECT * FROM artikel where categorie = 'bedankt'";
     }
 
-    $result = $dbh->query($sql);
-
-    if ($result) {
-        while ($row = $result->fetch_assoc()) {
-            echo "<div class='column1'>";
-            echo "<a href='bloem.php?id=" . $row["idartikel"] . "'>";
-            echo "<div class='card1'>";
-            echo "<img style='width: 200px; height: 200px; margin-left: -30px; margin-top: -20px; position: relative; float: left;' src='../default/images/" . $row["afbeelding"] . "'>";
-            echo "<h3 class='h3'>" . $row["naam"] . "</h3>";
-            echo "</div></a></div>";
+    if (!empty($sql)) {
+        $result = $dbh->query($sql);
+        if ($result) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<div class='column1'>";
+                echo "<a href='bloem.php?id=" . $row["idartikel"] . "'>";
+                echo "<div class='card1'>";
+                echo "<img style='width: 200px; height: 200px; margin-left: -30px; margin-top: -20px; position: relative; float: left;' src='../default/images/" . $row["afbeelding"] . "'>";
+                echo "<h3 class='h3'>" . $row["naam"] . "</h3>";
+                echo "</div></a></div>";
+            }
         }
     }
     ?>
 </div>
 </body>
 </div>
-</html>
