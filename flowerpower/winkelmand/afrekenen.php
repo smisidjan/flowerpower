c486b33f19778b1ae02cbda7d776133d63c8b908
Sarai Misidjan <smisidjan@roc-dev.com>
2021-04-07 13:36:02 +0200

    afrekenen pagina

--- flowerpower/winkelmand/afrekenen.php
+++ flowerpower/winkelmand/afrekenen.php
@@ -22,9 +22,11 @@ if (isset($_POST['submit'])) {
                         <h2 class="card-title" style="float:left; margin-left: 22px; margin-bottom: 50px;">
                             Bezorging</h2>
                         <div class="card-text" style="margin-top: 50px;">
+                            <br>
                             <form action="index.php" method="post">
+                                <br>
                                 <div class="row">
-                                    <div class="col-6" style="width: 100%;">
+                                    <div class="col-6">
                                         <div class="input-group">
                                             <span class="input-group-addon"></span>
                                             <input type="text" class="form-control" name="naam" placeholder="Naam"
@@ -94,6 +96,7 @@ if (isset($_POST['submit'])) {
                                     </div>
                                 </div>
                                 <br>
+                                <div class="row">
                                 <div class="col-12">
                                     <div class="input-group">
                                         <span class="input-group-addon"></span>
@@ -101,7 +104,8 @@ if (isset($_POST['submit'])) {
                                                placeholder="Datum" required>
                                     </div>
                                 </div>
-                                <hr class="solid">
+                                </div>
+                                <hr class="solid" style="margin-top: 30px;">
                                 <div class="col-4" style="float: right">
                                     <div class="form-group">
                                         <!-- Button trigger modal -->
@@ -124,36 +128,10 @@ if (isset($_POST['submit'])) {
 
                             <div class="row" style="margin-top: 100px">
                                 <div class="column" style="width: 50%;">
-                                    <div class="card rounded"
-                                         style="height: 300px; border: 1px solid #FF6F83; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
-                                    </div>
                                 </div>
                                 <div class="column" style="width: 50%">
                                     <div class="card-body" style="text-align: left">
-                                        <?php
-                                        require "../default/dbh.php";
 
-                                        $sqlHoofd = "select * from winkel where naam = 'Hoofdkantoor'";
-                                        $sqlAms = "select * from winkel where naam = 'Stadionplein'";
-                                        $sqlDen = "select * from winkel where naam = 'Den Haag CS'";
-
-                                        if ($result = $dbh->query($sqlHoofd) or $result = $dbh->query($sqlAms) or $result = $dbh->query($sqlDen)) {
-                                            while ($row = $result->fetch_assoc()) {
-                                                echo "<h4 class='card-title' style='font-size: 19px; font-weight: bold;'>FlowerPower! " . $row['naam'] . "</h4>";
-                                                echo "<p class='card-text' style='margin-top: 40px; font-size: 15px;'>" . $row['adres'] . " " . $row['huisnummer'] . "</p>";
-                                                echo "<p class='card-text' style='font-size: 17px; margin-top: -8px;'>" . $row['postcode'] . " " . $row['plaats'] . "</p>";
-                                                echo "<p class='card-text' style='font-size: 17px; margin-top: -8px;'>" . $row['email'] . "</p>";
-                                                echo "<p class='card-text' style='font-size: 17px; margin-top: -8px;'>" . $row['telefoon'] . "</p>";
-                                            }
-                                            $result->free_result();
-                                        }
-                                        ?>
-                                        <span><h4 class="card-title"
-                                                  style="margin-top: 40px; font-size: 18px; font-weight: bold;">Openingstijden</h4></span>
-                                        <p class="card-text" style="margin-bottom: -6px; font-size: 17px;">Ma/za -<span> 8:30 t/m 18:00</span>
-                                        </p>
-                                        <p class="card-text" style="font-size: 17px;">Zo - <span>gesloten</span></p>
-                                        </p>
                                     </div>
                                 </div>
                             </div>
